02878ae99d88b1abce0b4b7d7f93aad22371381f
JazzPants <leighton.l14@gmail.com>
2021-11-09 08:49:32 +1100

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2,4); 
+}
+//or (2,4), which is index 2 and 3
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+//let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
