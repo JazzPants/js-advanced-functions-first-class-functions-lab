ef9ecdbee7db649bce0b4b9648b3dbe5c2531037
JazzPants <leighton.l14@gmail.com>
2021-11-09 17:36:46 +1100

    Done

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 // Code your solution in this file!
 const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0,2);
+const returnFirstTwoDrivers = function(x) { //x replaced by "drivers" array
+    return x.slice(0,2);
 }
 
-const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(2,4); 
+const returnLastTwoDrivers = function(x) {
+    return x.slice(2,4); 
 }
 //or (2,4), which is index 2 and 3
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 //let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function(multiplierValue) {
+    return function(x) {
+        return multiplierValue * x;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2); //faredoubler() is a function which passes a value of 2 to createFareMultiplier
+
+const fareTripler = createFareMultiplier(3);
+
+console.log(fareDoubler(5));
+/*
+function fareDoubler() {
+    return createFareMultiplier(2);
+} 
+
+function fareTripler() {
+    return createFareMultiplier(3);
+}
+*/
+
+const selectDifferentDrivers = function(driversArray, theDriversToReturn) {
+    return theDriversToReturn(driversArray); //theDriversToReturn can be named anything, except function, it is the function argument (callback)
+}
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
+//theDriversToReturn inside selectDifferentDrivers changes to returnFirstTwoDrivers because it is a variable function
+//then we pass in the array called drivers
